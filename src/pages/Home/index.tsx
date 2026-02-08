9fdcad09cf0b8d0b9dbce84d9bcb8cc0b14bde13
lalitgawde <gawdelalit0@gmail.com>
2026-02-08 19:04:06 +0530

    My photos feature

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import Stories from "@/components/Stories";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import React from "react";
 
 const Home = () => {
   return (
